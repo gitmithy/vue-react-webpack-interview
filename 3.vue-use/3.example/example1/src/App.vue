24c497dbce277b3216848277b9a8212d80101de1
berrysmithy <yeah2109@yeah.net>
2020-06-04 17:54:39 +0800

    3-3final

---
+++ 3.vue-use/3.example/example1/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <ComponentsDemo/>
+    <!-- <ConditionDemo/> -->
+    <!-- <EventDemo/> -->
+
+    <!-- <ComponentDemo/> -->
+
+    <!-- <AdvancedUse/> -->
+
+    <!-- <CartDemo/> -->
+  </div>
+</template>
+
+<script>
+
+
+// 3.2
+// import TplDemo from './components/3.2TplDemo'index
+// 3.2
+// import ComputedDemo from './components/3.2ComputedDemo.vue'
+// 3.2
+import ComponentsDemo from './components/3.3ComponentsDemo/index'
+// import ConditionDemo from './components/BaseUse/ConditionDemo'
+// import EventDemo from './components/BaseUse/EventDemo'
+
+// import ComponentDemo from './components/ComponentsDemo/index'
+
+// import AdvancedUse from './components/AdvancedUse/index'
+
+// import CartDemo from './components/Cart/index'
+
+export default {
+  name: 'app',
+  components: {
+    ComponentsDemo
+    // HelloWorld
+    // EventDemo
+    // ComponentDemo
+    // AdvancedUse
+    // CartDemo
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
